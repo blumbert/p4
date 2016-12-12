3da04c20369ff7b622ea1359c4745af94c2f3e05
Billy <billy.lumbert@yahoo.com>
2016-12-12 18:55:39 -0500

    added routes for shoes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// shoe resource
+# Index page to show all the shoes
+Route::get('/shoes', 'ShoeController@index')->name('shoes.index');
+# Show a form to create a new shoe
+Route::get('/shoes/create', 'ShoeController@create')->name('shoes.create');
+# Process the form to create a new shoe
+Route::post('/shoes', 'ShoeController@store')->name('shoes.store');
+# Show an individual shoe
+Route::get('/shoes/{id}', 'ShoeController@show')->name('shoes.show');
+# Show form to edit a shoe
+Route::get('/shoes/{id}/edit', 'ShoeController@edit')->name('shoes.edit');
+# Process form to edit a shoe
+Route::put('/shoes/{id}', 'ShoeController@update')->name('shoes.update');
+# Delete a shoe
+Route::delete('/shoes/{id}', 'ShoeController@destroy')->name('shoes.destroy');
